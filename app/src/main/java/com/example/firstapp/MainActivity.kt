463bc9b67a722f8d131bbf3d04e3eeeed5e40be0
ulug04 <fayzulug7@gmail.com>
2022-10-19 22:18:08 -0700

    jhbuguy

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.annotation.SuppressLint
+import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,18 +11,70 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var canAddOperation = false
+    private var canAddDecimal = true
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+       fun numberAction (view: View)
+       {
+
+           if(view is Button)
+           {
+
+            if(view.text ==".")
+            {
+              if(canAddDecimal)
+
+               workingsTV.append(view.text)
+               canAddOperation = true
+
+               canAddDecimal = false
+           }
+               else
+
+                workingsTV.append(view.text)
+                canAddOperation = true
 
 
+           }
 
+
+       }
+       fun operationAction (view: View)
+       {
+           if(view is Button & canAddOperation)
+
+           {
+               workingsTV.append(view.text)
+               canAddOperation = false
+               canAddDecimal = true
+           }
+
+       }
+
+       fun allClearAction (view: View)
+
+       {
+
+           workingsTV.text= ""
+           resultsTV.text=""
+       }
+
+
+    fun backSpaceAction(view: View)
+    {
+        val length = workingsTV.lenght()
+        if(length > 0)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
 
     }
 
-    fun allClearAction(view: View) {}
-    fun backSpaceAction(view: View) {}
+
+
 
 
 }
