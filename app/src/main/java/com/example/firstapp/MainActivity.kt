ef87bc3f357db55b6a30805b4709dad6f27a36be
ulug04 <fayzulug7@gmail.com>
2022-10-26 20:49:45 -0700

    jhbuguy

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,54 +1,47 @@
 package com.example.firstapp
 
-import android.annotation.SuppressLint
-import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var canAddOperation = false
     private var canAddDecimal = true
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
-       fun numberAction (view: View)
-       {
+       fun numberAction (view: View) {
 
-           if(view is Button)
-           {
+           if(view is Button) {
 
-            if(view.text ==".")
-            {
+            if(view.text == ".") {
               if(canAddDecimal)
 
                workingsTV.append(view.text)
-               canAddOperation = true
 
                canAddDecimal = false
-           }
-               else
+
+           } else
 
                 workingsTV.append(view.text)
                 canAddOperation = true
 
-
            }
-
-
        }
-       fun operationAction (view: View)
-       {
-           if(view is Button & canAddOperation)
 
-           {
+
+       fun operationAction (view: View) {
+           if(view is Button && canAddOperation){
                workingsTV.append(view.text)
                canAddOperation = false
                canAddDecimal = true
@@ -56,23 +49,106 @@ class MainActivity : AppCompatActivity() {
 
        }
 
-       fun allClearAction (view: View)
-
-       {
+       fun allClearAction (view: View) {
 
            workingsTV.text= ""
            resultsTV.text=""
        }
 
 
-    fun backSpaceAction(view: View)
-    {
-        val length = workingsTV.lenght()
+    fun backSpaceAction(view: View){
+        val length = workingsTV.text.length
+
         if(length > 0)
             workingsTV.text = workingsTV.text.subSequence(0, length - 1)
 
     }
 
+    fun equalsAction(view: View){
+
+        resultsTV.text = calculateResults()
+    }
+
+
+    private fun  calculateResults(): String{
+
+        val digitsOperator = digitsOperation()
+        if(digitsOperator.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperator)
+        return ""
+    }
+
+    private fun  timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains ('/'))
+        {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedlist: MutableList<Any>): MutableList<Any> {
+
+        val newlist = mutableListOf<Any>()
+        var restartIndex = passedlist.size
+
+        for(i in passedlist.indices){
+
+            if(passedlist[i] is Char && i !=passedlist.lastIndex && i < restartIndex){
+
+                val operator = passedlist[i]
+                val prevDigit = passedlist[i]  as Float
+                val nextDigit = passedlist[i]  as Float
+                when(operator)
+
+                {
+                    'x' ->
+
+
+                    {
+                        newlist.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+
+                    {
+                        newlist.add(prevDigit / nextDigit)
+                    }
+                }
+
+            }
+        }
+
+
+        return newlist
+
+    }
+
+    private fun digitsOperation(): MutableList<Any>{
+
+        val list = mutableListOf<Any>()
+        var currentDigit= ""
+        for(character in workingsTV.text){
+
+            if(character.isDigit()|| character == '.' )
+                currentDigit += character
+
+            else{
+
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if (currentDigit !="")
+            list.add(currentDigit.toFloat())
+
+            return list
+    }
 
 
 
